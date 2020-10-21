376acb162d3c47d527054c02893ba0fd3c5629df
Myra-Grace <59889116+myra-grace@users.noreply.github.com>
2020-10-21 03:32:04 -0400

    contact form

--- client/src/pages/contact-page/ContactPage.js
+++ client/src/pages/contact-page/ContactPage.js
@@ -2,17 +2,35 @@ import React, { useEffect, useState, useRef } from "react";
 import './ContactPage.css';
 
 const ContactPage = () => {
+    const handleSendEmail = () => {
+        console.log("sent");
+    }
 
     return (
         <div className="contact-page-container">
-            <form className="contact-page-form"
-            netlify method="POST" name="EmailForm">
+            <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+            {/* <form className="contact-page-form"
+            netlify method="POST" name="contact">
                 <p>Email</p>
                 <input type="email" size="19" name="ContactName" required="required" /><br/>
                 <p>Message</p>
                 <textarea className="contact-textarea" name="ContactComment" required="required"></textarea><br/><br/>
-                <button type="submit" value="Submit"></button>
-            </form>
+                <button type="submit" value="Submit" onClick={handleSendEmail}>Send</button>
+            </form> */}
         </div>
     )
 }
