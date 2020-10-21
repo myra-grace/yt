2ca68a867f87d1d0af5fbb9716894758a8a6c1ac
Myra-Grace <59889116+myra-grace@users.noreply.github.com>
2020-10-21 02:45:28 -0400

    email

--- client/src/pages/contact-page/ContactPage.js
+++ client/src/pages/contact-page/ContactPage.js
@@ -6,9 +6,9 @@ const ContactPage = () => {
     return (
         <div className="contact-page-container">
             <form className="contact-page-form"
-            method="post" encType="multipart/form-data" name="EmailForm">
-                <p>Name</p>
-                <input type="text" size="19" name="ContactName" required="required" /><br/>
+            netlify name="EmailForm">
+                <p>Email</p>
+                <input type="email" size="19" name="ContactName" required="required" /><br/>
                 <p>Message</p>
                 <textarea className="contact-textarea" name="ContactComment" required="required"></textarea><br/><br/>
                 <input type="submit" value="Submit"></input>
